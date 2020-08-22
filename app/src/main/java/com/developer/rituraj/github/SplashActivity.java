65128a64fcc1c976f4577de67c205371b0c97682
Rituraj Jain <rrjain20122000@gmail.com>
2020-08-22 20:36:03 +0530

    Create Files

--- app/src/main/java/com/developer/rituraj/github/SplashActivity.java
+++ app/src/main/java/com/developer/rituraj/github/SplashActivity.java
@@ -1,4 +1,4 @@
-package com.developer.arsltech.github;
+package com.developer.rituraj.github;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+
+
 
 public class SplashActivity extends AppCompatActivity {
 
